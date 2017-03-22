e6e4059e2e7a20c41f188d5b8a90bf230c7df397
Alexander Antipin <alex@antipin.com>
2017-03-22 17:29:30 +0300

    Add spy function for leetcode test cases extraction

---
+++ utils/spy.js
@@ -0,0 +1,37 @@
+/**
+ * It spies for test cases that were passed to problem solutios and allows to dump it
+ * @param {Function} func
+ * @returns {Function}
+ */
+function spy(func) {
+
+    return function() {
+
+        const result = func.apply(null, arguments)
+
+        spy.cases.current += 1
+        spy.cases.dump.push({
+            input: Array.from(arguments),
+            output: result,
+        })
+
+        if (spy.cases.current === spy.cases.total) {
+
+            console.log(
+                JSON.stringify(spy.cases.dump, null, 4)
+            )
+
+            return null
+        }
+
+        return result
+
+    }
+
+}
+
+spy.cases = {
+    total: 151,
+    current: 0,
+    dump: []
+}
